eec0f7c441cacf82ed4e0c43469b3d49a2196e48
ibuetler <ivan.buetler@hacking-lab.com>
2023-10-24 13:49:50 +0200

    updating deploy flag scripts

--- generators/app/templates/flag-deploy-scripts/deploy-env-flag.sh
+++ generators/app/templates/flag-deploy-scripts/deploy-env-flag.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/with-contenv bash
 # Use this script to write / replace the goldnugget to the correct place.
 # The goldnugget is available as $GOLDNUGGET.
+
+# Your task
+echo "please store $GOLDNUGGET to the final destination"
+
+
--- generators/app/templates/flag-deploy-scripts/deploy-file-flag.sh
+++ generators/app/templates/flag-deploy-scripts/deploy-file-flag.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/with-contenv bash
 # Use this script to copy the goldnugget file to the correct place.
 # You can find it under /goldnugget/<%= uuid %>.gn
+
+echo "put your commands to deploy the file based flag here"
+echo "the /goldnugget/*.gn contains the flag"
+
+GN_FILE=$(ls /goldnugget/*.gn)
+source $GN_FILE
+
+echo "extend this script and move $GOLDNUGGET to the destination you want"
